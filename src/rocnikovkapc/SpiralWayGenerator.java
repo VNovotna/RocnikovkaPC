fd2b77603d036e3b0e79bcf51c41a3f2fb6aa492
Petr Poliak <petrpoliak9gmail.com>
2013-05-02 15:12:01 +0200

    SpiralWayGenerator

--- src/rocnikovkapc/SpiralWayGenerator.java
+++ src/rocnikovkapc/SpiralWayGenerator.java
@@ -21,37 +21,69 @@ public class SpiralWayGenerator {
     }
 
     public Waypoint generateNextWaypoint(Waypoint previous, Waypoint actual) {
-        if (Math.abs(previous.x - actual.x) < Math.abs(previous.y - actual.y)) {
-            if (previous.y < actual.y) {
-                
+        if (Math.abs(previous.x - actual.x) < Math.abs(previous.y - actual.y)) { //pohyb po ose x/y
+            //pohyb po y
+            if (previous.y < actual.y) {//pohyb nahoru/dolu
+                //nahoru
+                long limit = YRANGE - Math.round(actual.x / (TRACK_WIDTH / 2)) * (TRACK_WIDTH / 2);
+                if (Math.abs(limit - actual.y) < 50) {//kontrola jestli neni na/blízko limitu
+                    return right(actual);
+                }
+                return up(actual);
             } else {
-                
+                //pohyb dolu
+                long limit = Math.round((XRANGE - actual.x) / (TRACK_WIDTH / 2)) * (TRACK_WIDTH / 2);
+                if (Math.abs(limit - actual.y) < 50) {//kontrola jestli neni na/blízko limitu
+                    return left(actual);
+                }
+                return down(actual);
             }
         } else {
-            if (previous.x < actual.x) {
-                
+            //pohyb po ose x
+            if (previous.x < actual.x) {//pohyb doprava/doleva
+                //pohyb doprava
+                long limit = XRANGE - Math.round((YRANGE - actual.y) / (TRACK_WIDTH / 2)) * (TRACK_WIDTH / 2);
+                if (Math.abs(limit - actual.x) < 50) {//kontrola jestli neni na/blízko limitu
+                    return down(actual);
+                }
+                return right(actual);
             } else {
-                
+                //pohyb doleva
+                long limit = STEP_LENGTH + Math.round(actual.y / (TRACK_WIDTH / 2)) * (TRACK_WIDTH / 2);
+                if (Math.abs(limit - actual.x) < 50) {//kontrola jestli neni na/blízko limitu
+                    return up(actual);
+                }
+                return left(actual);
             }
 
         }
+    }
 
-        return null;
+//pohyb směrem dolu
+    public Waypoint down(Waypoint actual) {
+        long limit = Math.round((XRANGE - actual.x) / (TRACK_WIDTH / 2)) * (TRACK_WIDTH / 2);
+        long y = (long) Math.max(limit, actual.y - STEP_LENGTH);
+        return new Waypoint(actual.x, y);
     }
-    public Waypoint down(Waypoint actual){
-        
-        return null;
+
+//pohyb směrem nahoru
+    public Waypoint up(Waypoint actual) {
+        long limit = YRANGE - Math.round(actual.x / (TRACK_WIDTH / 2)) * (TRACK_WIDTH / 2);
+        long y = (long) Math.min(limit, actual.y + STEP_LENGTH);
+        return new Waypoint(actual.x, y);
     }
-    public Waypoint up(Waypoint actual){
-        
-        return null;
+
+//pohyb směrem doprava
+    public Waypoint right(Waypoint actual) {
+        long limit = XRANGE - Math.round((YRANGE - actual.y) / (TRACK_WIDTH / 2)) * (TRACK_WIDTH / 2);
+        long x = (long) Math.min(limit, actual.x + STEP_LENGTH);
+        return new Waypoint(x, actual.y);
     }
-    public Waypoint right(Waypoint actual){
-        
-        return null;
-    }
-    public Waypoint left(Waypoint actual){
-        
-        return null;
+
+//pohyb směrem doleva
+    public Waypoint left(Waypoint actual) {
+        long limit = STEP_LENGTH + Math.round(actual.y / (TRACK_WIDTH / 2)) * (TRACK_WIDTH / 2);
+        long x = (long) Math.max(limit, actual.x - STEP_LENGTH);
+        return new Waypoint(x, actual.y);
     }
 }
