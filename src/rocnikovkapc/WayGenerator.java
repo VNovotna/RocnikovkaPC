151087a1fec9fe4d2d0365eb08a256aff2cd6f61
Petr Poliak <petrpoliak9gmail.com>
2013-05-08 15:21:10 +0200

    ladění wayGeneratoru

--- src/rocnikovkapc/WayGenerator.java
+++ src/rocnikovkapc/WayGenerator.java
@@ -23,13 +23,13 @@ public class WayGenerator {
     public Waypoint gnw(Waypoint actual) {
         boolean nahoru = Math.round(((double)actual.x) / (TRACK_WIDTH / 2.0)) == 1 || Math.round(actual.x / (TRACK_WIDTH / 2)) % 5 == 0 ? true : false;
         if (nahoru) {
-            if (Math.abs(YRANGE - actual.y) < TRACK_WIDTH * 1.25) {
+            if (Math.abs(YRANGE - actual.y) < (TRACK_WIDTH/2.0) * 1.25) {
                 return new Waypoint(Math.min(XRANGE - TRACK_WIDTH, actual.x + TRACK_WIDTH), actual.y);
             } else {
                 return new Waypoint(actual.x, Math.min(YRANGE - TRACK_WIDTH, actual.y + STEP_LENGTH));
             }
         } else {
-            if (actual.y < TRACK_WIDTH * 1.25) {
+            if (actual.y < (TRACK_WIDTH/2.0) * 1.25) {
                 return new Waypoint(Math.min(XRANGE - TRACK_WIDTH, actual.x + TRACK_WIDTH), actual.y);
             } else {
                 return new Waypoint(actual.x, Math.max(TRACK_WIDTH, actual.y - STEP_LENGTH));
