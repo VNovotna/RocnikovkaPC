a509eb76097fbe768eaf4b1f3ca9b67991b7a810
viky <viky@G580-viky>
2013-05-19 21:42:59 +0200

    unused imports

--- src/nePrintVerze/TestingPanel.java
+++ src/nePrintVerze/TestingPanel.java
@@ -1,6 +1,5 @@
 package nePrintVerze;
 
-import rocnikovkapc.*;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Point;
@@ -8,8 +7,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JPopupMenu;
